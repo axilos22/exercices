fd2dd090357f90afcec713b502e872b42c5d50ff
Axilos <zenos_22@hotmail.com>
2024-07-14 18:49:22 +0200

    Update pyramid.py

--- python/src/pyramid/pyramid.py
+++ python/src/pyramid/pyramid.py
@@ -1,10 +1,12 @@
 from sys import argv
 
-def draw_pyramid(height: int, symbol: str='*') -> None:
-    for line in range(1, height+1):
+
+def draw_pyramid(height: int, symbol: str = '*') -> None:
+    for line in range(1, height + 1):
         for col in range(0, line):
             print(symbol + " ", end="")
         print("")
+
 
 if __name__ == "__main__":
     try:
